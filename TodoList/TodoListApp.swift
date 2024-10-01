756715c38de6cd9a306106a5c789374dcf93a42f
mulgir-govind <govind.mulgir.17@gmail.com>
2024-10-01 23:42:35 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Govind on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
